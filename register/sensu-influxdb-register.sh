9548501c2a3e08e88cd37d8276442dc4d698f4b9
A Rameshkumar <a.rameshkumar@ericsson.com>
2018-09-11 23:18:29 +0530

    register script added

--- register/sensu-influxdb-register.sh
+++ register/sensu-influxdb-register.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/dumb-init sh
 
-echo before configure
 sensuctl configure --non-interactive --url=http://sensu-backend:8080 --username=admin --password=P@ssw0rd! --format=tabular 
 
-echo after configure  
+sensuctl user change-password admin --current-password=P@ssw0rd! --new-password $1
 
-sensuctl handler create sensu-influxdb-handler --command "sensu-influxdb-handler --addr http://influxdb:8086 --username metrics --password metrics --db-name metrics" --type pipe
-
-echo after configure
+sensuctl handler create sensu-influxdb-handler --command "sensu-influxdb-handler --addr http://influxdb:8086 --username metrics --password $2 --db-name metrics" --type pipe
